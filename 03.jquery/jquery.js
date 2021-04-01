cd5fee94cd160cec5d75b9c0c1cf3d609418ab3e
YJun1364 <didvkzh1@gmail.com>
2021-04-01 09:11:12 +0900

    first commit

---
+++ 03.jquery/jquery.js
@@ -0,0 +1,79 @@
+$(function () {
+  //jstree plugin
+  var myTree = $("#tree").jstree({
+    checkbox: {
+      keep_selected_style: false,
+    },
+    plugins: ["checkbox", "search", "wholerow"], // checkbox 와 search 플러그인 적용 whoerow 는 칸 나누는 효과
+    core: { data: parms },
+  });
+
+  // Dialog
+  $("#list").dialog({
+    autoOpen: false, // 숨기기
+    modal: true, //모달대화상자
+    position: "center",
+    show: {
+      duration: 500,
+    },
+    hide: {
+      duration: 500,
+    },
+  });
+
+  // Dialog-open
+  $("#open-btn").on("click", function () {
+    $("#list").dialog("open");
+    $("#open-btn").hide();
+    $("#tree").jstree("open_all"); // 편의상 삽입 코드 - 모든 노드 open
+  });
+
+  // close-btn (좌측 상단)
+  $(".ui-dialog-titlebar-close").on("click", function () {
+    $("#open-btn").show(500);
+    $("#tree").jstree("deselect_all");
+  });
+
+  // input : search 기능
+  $("#search").keyup(function () {
+    var to = false;
+    if (to) {
+      clearTimeout(to);
+    }
+    to = setTimeout(function () {
+      var v = $("#search").val();
+      $("#tree").jstree(true).search(v);
+    }, 250);
+  });
+
+  //enter
+  $("#search").keyup(function (key) {
+    if (key.keyCode == 13) {
+      // //키의 코드가 13번일 경우 (13번은 엔터키)
+      $("#tree").jstree("deselect_all"); // checked 초기화
+      $("#tree").find(".jstree-search").trigger("click"); // 선택
+    }
+  });
+
+  // 취소 - jstree reset
+  $("#reset").on("click", function () {
+    $("#tree").jstree("deselect_all");
+  });
+
+  // 확인 - alert 창  정보 출력
+  $("#submit").on("click", function () {
+    var nodeID = $("#tree").jstree("get_checked"); //jstree('get_checked') : 체크된 노드의 id 값을 배열로 받아온다 nodeID 는 배열
+    console.log(nodeID.length);
+    if (nodeID.length == 0) {
+      alert("Are you checked?");
+    } else {
+      var message = "";
+      $.each(nodeID, function (index, items) {
+        // each 문 첫번째 인자 : 배열 객체 / 두번째 인자 콜백함수(index,item)
+        var nodeInfo = $("#tree").jstree("get_node", items).text; //jstree('get_node',nodeId) - 노드 id 로 노드정보를 검색하는 메소드
+        message += nodeInfo + "\n";
+      });
+      alert(message);
+    }
+  });
+});
